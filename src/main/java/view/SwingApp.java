884836bab9fef11a6b5ae7617dfef37993589db7
Ronald-ICastroG_Acer <ronaldivan.1210868@correo.uis.edu.co>
2023-12-21 20:08:43 -0500

    RefreshTableCompleta

--- src/main/java/view/SwingApp.java
+++ src/main/java/view/SwingApp.java
@@ -2,14 +2,18 @@ package view;
 
 import Repository.Repository;
 import model.Employee;
+import Repository.EmployeeRepository;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.sql.SQLException;
 
 public class SwingApp extends JFrame {
 
 
     private final JTable employeeTable;
+    private final Repository<Employee> employeeRepository;
 
     public SwingApp(){
         //configurar la ventana
@@ -48,5 +52,65 @@ public class SwingApp extends JFrame {
         eliminarButton.setFocusPainted(false);
 
 
+        //Crear el objeto repository para acceder a la base de datos
+        employeeRepository=new EmployeeRepository();
+
+
+        //Agregar ActionListener para los botones
+        agregarButton.addActionListener(e->{
+            try{
+                agregarEmpleado();
+
+            }catch(SQLException ex){
+                throw new RuntimeException(ex);
+            }
+        });
+
+        actualizarButton.addActionListener(e->actulizarEmpleado());
+        eliminarButton.addActionListener(e->eliminarEmpleado());
+
+    }
+
+
+    private void refreshEmployeeTable(){
+        //Obtener la lista actualizada de empleados desde la bd
+        try{
+            List<Employee> employees=employeeRepository.findAll();
+
+            //Crear un modelo de tabla y establecer los datos de los empleados
+            DefaultTableModel model=new DefaultTableModel();
+            model.addColumn("ID");
+            model.addColumn("Nombre");
+            model.addColumn("Apellido Paterno");
+            model.addColumn("Apellido Materno");
+            model.addColumn("Email");
+            model.addColumn("Salario");
+
+            for(Employee employee: employees){
+                Object[] rowData={
+                        employee.getEmpId(),
+                        employee.getEmpFirstName(),
+                        employee.getEmpPSurName(),
+                        employee.getEmpMSurName(),
+                        employee.getEmpEmail(),
+                        employee.getEmpSalary()
+                };
+                model.addRow(rowData);
+
+
+            }
+            //establecer el modelo de tabla actualizado
+            employeeTable.setModel(model);
+            }catch(SQLException e){
+            JOptionPane.showMessageDialog(this,"Error al obtener los datos de la bd");
+
+
+        }
+
+
+    }
+
+    private void agregarEmpleado(){
+        
     }
 }
